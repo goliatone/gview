20b63056363398e1ab2d1ffa2fc024a95652337a
goliatone <burgosemi@gmail.com>
2014-03-04 23:55:03 -0500

    Example: main.js, update paths. Setup view.

--- examples/main.js
+++ examples/main.js
@@ -2,13 +2,43 @@
 /* jshint strict: false */
 requirejs.config({
     paths: {
-        'jquery': '../lib/jquery/jquery',
-        'gview': '../src/gview'
+        'jquery': 'jquery/jquery',
+        'gview': 'gview'
     }
 });
 
 define(['gview', 'jquery'], function (Gview, $) {
     console.log('Loading');
-	var gview = new Gview();
-	gview.init();
+	var gview = new Gview({
+		id:'gview',
+		tagName:'button',
+		className:'button',
+		peperone:function(){
+			console.log('hola');
+		},
+		events:{
+			'click':'peperone'
+		},
+		render:function(){
+			console.log(this);
+			this.el.innerHTML = 'hola';
+			return this;
+		}
+
+	}).render();
+
+	var profile = new Gview({
+		el:$('.profile'),
+		events:{
+			'click':function(){
+				console.log('click')
+			}
+		},
+		render:function(){
+			this.el.innerHTML = '<p>This should be rendered as it is</p>';
+		}
+	}).render();
+	
+	$('aside').append(gview.el);
+	window.view = gview;
 });
